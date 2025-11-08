c59ebb09e568166f5a82fd780e6654f08f54dc9e
Henrique Maciel <164427294+Maciel0123@users.noreply.github.com>
2025-11-08 20:32:20 -0300

    Update PatioController.java

--- src/main/java/fiap/com/br/future_stack/patio/PatioController.java
+++ src/main/java/fiap/com/br/future_stack/patio/PatioController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,6 +46,7 @@ public class PatioController {
         return patioService.createPatio(dto);
     }
 
+    @Transactional
     @PutMapping("/{id}")
     @Operation(summary = "Atualizar pátio", tags = "Patio")
     public PatioDTO atualizar(@PathVariable Long id, @RequestBody @Valid PatioDTO dto) {
